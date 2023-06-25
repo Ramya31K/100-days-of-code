2b48f3860008c24d6a7054013f96481c99aebc2f
Ramya31K <137478115+Ramya31K@users.noreply.github.com>
2023-06-25 09:09:39 +0530

    Create Chef and chapters.py

---
+++ Chef and chapters.py
@@ -0,0 +1,7 @@
+X = int(input("Enter the number of courses: "))
+Y = int(input("Enter the number of units per course: "))
+Z = int(input("Enter the number of chapters per unit: "))
+
+total_chapters = X * Y * Z
+
+print("Total number of chapters Chef has to study this semester:", total_chapters)
