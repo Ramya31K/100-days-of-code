dce55013de7833a7d16a4265b28ec7d04e7277e3
Ramya31K <137478115+Ramya31K@users.noreply.github.com>
2023-06-27 08:50:10 +0530

    Create Find remainder.py

---
+++ Find remainder.py
@@ -0,0 +1,7 @@
+def remainder(x,y):
+ remain=x%y
+ return remain
+x=int(input("enter a x value:"))
+y=int(input ("enter a y value:"))
+find_remain=remainder(x,y)
+print("the remainder of", x,"divided by",y,"is",find_remain)
